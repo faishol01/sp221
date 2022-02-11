927df117bc2cc03fde8e3e359d3728c650d41471
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2022-02-11 21:29:02 +0700

    Week 0

---
+++ TXT/myscript.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+FILES="my*"
+SHA="SHA256SUM"
+
+echo "rm -f $SHA"
+rm -f $SHA*
+
+echo "sha256sum $FILES > $SHA"
+sha256sum $FILES > $SHA
+
+echo "sha256sum -c $SHA"
+sha256sum -c $SHA
+
+echo "gpg -o $SHA.asc -a -sb $SHA"
+gpg -o $SHA.asc -a -sb $SHA
+
+echo "gpg --verify $SHA.asc $SHA"
+gpg --verify $SHA.asc $SHA
